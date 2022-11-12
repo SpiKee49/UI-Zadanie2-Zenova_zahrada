1349b83d18a34884ced1bf1a0eab4e8299624d3a
René Bukovina <r.bukovina@gmail.com>
2022-11-12 13:39:34 +0100

    Zeby to maria skarala

--- index.ts
+++ index.ts
@@ -12,7 +12,6 @@ type Population = Array<Individual>
 //mapa
 let map = Array(12).fill(Array(10).fill('_'))
 
-console.log('map keys', map.keys())
 //Starting map print
 function printMap(map: Grid) {
     let mapToPrint: Grid = []
@@ -23,7 +22,15 @@ function printMap(map: Grid) {
         mapToPrint.push(row)
     }
 
-    mapToPrint.forEach((item) => console.log(item.join(' ')))
+    mapToPrint.forEach((item) => {
+        console.log(
+            item
+                .map((item) =>
+                    item != '_' && +item >= 10 ? `${item} ` : ` ${item} `
+                )
+                .join('')
+        )
+    })
 }
 
 //starting positions array
@@ -44,32 +51,51 @@ function fitness(input: Individual) {
             decisions: [...gen.decisions],
         }
     })
-
+    let dontBotherChecking: boolean = false
     let grind: Grid = map.map((item) => [...item])
-    let score: number = 0
+    let numberOfTiles: number = 0
 
     let direction: string
     //take one gen a try to make move
-    individual.forEach((gen) => {
-        if (gen.position <= 12) {
+    for (let i = 0; i < individual.length; i++) {
+        if (dontBotherChecking) {
+            break
+        }
+
+        if (individual[i].position <= 12) {
             //will be going down
             direction = 'DOWN'
-        } else if (gen.position > 12 && gen.position <= 20) {
+        } else if (
+            individual[i].position > 12 &&
+            individual[i].position <= 20
+        ) {
             //will be going left mainly
             direction = 'LEFT'
-        } else if (gen.position > 20 && gen.position <= 32) {
+        } else if (
+            individual[i].position > 20 &&
+            individual[i].position <= 32
+        ) {
             //will be going up
             direction = 'UP'
-        } else if (gen.position > 32 && gen.position <= 40) {
+        } else if (
+            individual[i].position > 32 &&
+            individual[i].position <= 40
+        ) {
             //will be going right mainly
             direction = 'RIGHT'
         }
 
-        movement(gen, direction, grind, score)
-    })
-    printMap(grind)
+        const { stuckOrDone, score } = movement(
+            individual[i],
+            direction,
+            grind,
+            i + 1
+        )
+        dontBotherChecking = stuckOrDone
+        numberOfTiles += score
+    }
 
-    return score
+    return numberOfTiles
 }
 
 //movement function
@@ -77,9 +103,9 @@ function movement(
     gen: Gen,
     initialDirection: string,
     globalMap: Grid,
-    score: number
+    index: number
 ) {
-    let iter: number = 1
+    let score: number = 0
     let x: number = 0
     let y: number = 0
     let direction: string = initialDirection
@@ -96,82 +122,82 @@ function movement(
         y = 9
     } else if (
         initialDirection == 'LEFT' &&
-        globalMap[11][13 - (gen.position - 1)] === '_'
+        globalMap[11][gen.position - 11] === '_'
     ) {
         x = 11
-        y = 13 - (gen.position - 1)
+        y = gen.position - 11
     } else if (
         initialDirection == 'RIGHT' &&
-        globalMap[0][40 - gen.position - 1] === '_'
+        globalMap[0][40 - (gen.position - 1)] === '_'
     ) {
         y = 40 - (gen.position - 1)
     } else {
         //if we cant start in occupied position
-        return 0
+        console.log('Skipped')
+        console.log({
+            index,
+            pos: gen.position,
+            direction,
+            x,
+            y,
+        })
+        return { stuckOrDone: false, score }
     }
-
+    console.log('\n----------------\n')
     console.log({
-        direction,
         pos: gen.position,
+        direction,
         x,
         y,
     })
-    console.log('globalMap[x][y]: ', globalMap[x][y])
 
     // make all the moves for current gen
     while (stuckOrDone != true) {
         if (globalMap[x][y] == '_') {
             //next tile is empty, we can put number of current gen in there
-            globalMap[x][y] = iter.toString()
+            globalMap[x][y] = index.toString()
 
             score += 1
         } else {
-            switch (direction) {
-                case 'DOWN':
-                case 'UP':
-                    if (
-                        x - 1 >= 0 &&
-                        globalMap[x - 1][y] == '_' &&
-                        x + 1 < globalMap.length &&
-                        globalMap[x + 1][y] == '_'
-                    ) {
-                        direction =
-                            gen.decisions.shift() == 'L' ? 'LEFT' : 'RIGHT'
-                    } else if (x - 1 >= 0 && globalMap[x - 1][y] == '_') {
-                        direction = 'LEFT'
-                    } else if (
-                        x + 1 < globalMap.length &&
-                        globalMap[x + 1][y] == '_'
-                    ) {
-                        direction = 'RIGHT'
-                    } else {
-                        stuckOrDone = true
-                    }
-                    break
-                case 'RIGHT':
-                case 'LEFT':
-                    if (
-                        y - 1 >= 0 &&
-                        globalMap[x][y - 1] == '_' &&
-                        y + 1 < globalMap[x].length &&
-                        globalMap[x][y + 1] == '_'
-                    ) {
-                        direction = gen.decisions.shift() == 'L' ? 'DOWN' : 'UP'
-                    } else if (y - 1 >= 0 && globalMap[x][y - 1] == '_') {
-                        direction = 'DOWN'
-                    } else if (
-                        y + 1 < globalMap[x].length &&
-                        globalMap[x][y + 1] == '_'
-                    ) {
-                        direction = 'UP'
-                    } else {
-                        stuckOrDone = true
-                    }
-                    break
+            if (direction === 'DOWN' || direction === 'UP') {
+                if (
+                    x - 1 >= 0 &&
+                    globalMap[x - 1][y] == '_' &&
+                    x + 1 < globalMap.length &&
+                    globalMap[x + 1][y] == '_'
+                ) {
+                    direction = gen.decisions.shift() == 'L' ? 'LEFT' : 'RIGHT'
+                } else if (x - 1 >= 0 && globalMap[x - 1][y] == '_') {
+                    direction = 'LEFT'
+                } else if (
+                    x + 1 < globalMap.length &&
+                    globalMap[x + 1][y] == '_'
+                ) {
+                    direction = 'RIGHT'
+                } else {
+                    stuckOrDone = true
+                }
+            } else {
+                if (
+                    y - 1 >= 0 &&
+                    globalMap[x][y - 1] == '_' &&
+                    y + 1 < globalMap[x].length &&
+                    globalMap[x][y + 1] == '_'
+                ) {
+                    direction = gen.decisions.shift() == 'L' ? 'DOWN' : 'UP'
+                } else if (y - 1 >= 0 && globalMap[x][y - 1] == '_') {
+                    direction = 'DOWN'
+                } else if (
+                    y + 1 < globalMap[x].length &&
+                    globalMap[x][y + 1] == '_'
+                ) {
+                    direction = 'UP'
+                } else {
+                    stuckOrDone = true
+                }
             }
         }
         // depending on direction we move to next tile
-
         if (direction === 'DOWN') {
             y += 1
         } else if (direction === 'UP') {
@@ -179,11 +205,11 @@ function movement(
         } else if (direction === 'RIGHT') {
             x += 1
         } else if (direction === 'LEFT') {
-            y -= 1
+            x -= 1
         }
-
-        iter += 1
     }
+    printMap(globalMap)
+    return { stuckOrDone, score }
 }
 
 function main() {
